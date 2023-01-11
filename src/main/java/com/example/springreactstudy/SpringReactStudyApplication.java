7b1052ed9f7bf66cacbd8586e388f16339fca679
arodia <qeyiop12@naver.com>
2023-01-11 10:49:20 +0900

    2023.01.11 Spring, React 공부

---
+++ src/main/java/com/example/springreactstudy/SpringReactStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springreactstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springreactstudy/SpringReactStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springreactstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
